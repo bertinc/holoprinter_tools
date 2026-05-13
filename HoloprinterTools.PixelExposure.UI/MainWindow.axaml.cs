d92e67253d952e5d0d1bf17ba6ffcc892a99e706
bertinc <bertinc@gmail.com>
2026-05-12 20:30:52 -0600

    initial files push

---
+++ HoloprinterTools.PixelExposure.UI/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HoloprinterTools.PixelExposure.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
